f57849d54496b0a5fbaae2afe8312b4022e1f94b
J-Potes-GLB <juan.potes@globant.com>
2024-03-13 17:33:55 -0500

    Added driver setup and closing in the BaseTest class

--- src/test/java/com/herokuapp/theinternet/utils/tests/BaseTest.java
+++ src/test/java/com/herokuapp/theinternet/utils/tests/BaseTest.java
@@ -1,4 +1,24 @@
 package com.herokuapp.theinternet.utils.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+    protected WebDriver driver = null;
+
+    public void setDriver(){
+        driver = new ChromeDriver();
+    }
+
+    @BeforeMethod
+    public void setup(){
+        setDriver();
+    }
+
+    @AfterMethod
+    public void close(){
+        driver.close();
+    }
 }
